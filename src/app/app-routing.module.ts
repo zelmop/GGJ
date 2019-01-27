c807991c680b3ebf4ecd4306c669568ec7abc2b9
Zelmar Ramirez <zelmar.ramirez@gmail.com>
2019-01-26 22:51:32 -0300

    win component, text component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { StartComponent } from './components/start/start.component';
 import { TextoComponent } from './components/texto/texto.component';
+import { WinComponent } from './components/win/win.component';
 
 const routes: Routes = [
   { path: 'start', component: StartComponent },
   { path: 'texto', component: TextoComponent },
   { path: 'home', component: MainComponent },
+  { path: 'win', component: WinComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: MainComponent }
 ];
--- src/app/components/texto/texto.component.ts
+++ src/app/components/texto/texto.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, useAnimation } from '@angular/animations';
-import { bounce, flash, fadeInLeft } from 'ng-animate';
+import { bounce, flash, fadeInLeft, pulse } from 'ng-animate';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
     trigger('bounce', [ transition('* => *', useAnimation(bounce, {params: { timing: 3, delay: 0.5, a:'-30px', b: '25px', c: '-5px', d: '15px' }}))]),
     trigger('flash', [transition('* => *', useAnimation(flash, { params: {timing: 5, delay: 0.5}}))]),
     trigger('fadeInLeft', [transition('* => *', useAnimation(fadeInLeft, { params: {timing: 5, delay: 0.5}}))]),
-
+    trigger('pulse', [transition('* => *', useAnimation(pulse, {params: { timing: 2, delay: 0.5}}))])
   ]
 })
 
@@ -20,6 +20,7 @@ export class TextoComponent implements OnInit {
   public bounce: any;
   public flash: any;
   public fadeInLeft: any;
+  public pulse: any;
 
   public continue: boolean;
 
